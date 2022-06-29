7db042ba0e791d143e9772fd47b8a9b501cd84d3
bsesic <Benjamin-777@gmx.de>
2022-06-29 11:54:39 +0200

    Gidal image archive.

---
+++ gidal/scripts/generator.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Generator for the Gidal Image Archive. http://www.steinheim-institut.de/wiki/index.php/Archive:Gidal-Bildarchiv
+By Benjamin Schnabel, 2022.
+schnabel@hdm-stuttgart.de
+# Licence of the data: CC-BY-SA 4.0
+# https://creativecommons.org/licenses/by-sa/4.0/
+"""
+
+# get beacon file
