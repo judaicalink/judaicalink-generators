19a1ebd1a0009720452f35d553b08308d6442b70
bsesic <Benjamin-777@gmx.de>
2025-11-27 18:13:01 +0100

    generator rdf script updated

--- generator/rdf.py
+++ generator/rdf.py
@@ -1,9 +1,9 @@
 # generator/rdf.py
-from rdflib import Namespace, Graph, URIRef, Literal
+from rdflib import Namespace
 from rdflib.namespace import RDF, RDFS, XSD, DC, DCTERMS, SKOS, FOAF
 
 JL_DATA = Namespace("https://data.judaicalink.org/data/")
-JL_DS   = Namespace("https://data.judaicalink.org/datasets/")
+JL_DS = Namespace("https://data.judaicalink.org/datasets/")
 JL_ONTO = Namespace("https://data.judaicalink.org/ontology/")
 
 __all__ = ["JL_DATA", "JL_DS", "JL_ONTO", "RDF", "RDFS", "XSD", "DC", "DCTERMS", "SKOS", "FOAF"]
